a25f3040d55a3bc2cc5872e2fa3960323803bccb
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-01-25 22:20:03 +0200

    making the basic structure for the assignment

---
+++ products/products.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const productsSchema = require('./products-schema.js')
+
+class Products {
+    constructor() {
+    }
+
+    get() { }
+
+    create() { }
+
+    update() { }
+
+    delete() { }
+
+}
+
+module.exports = Products
